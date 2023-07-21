789925caaf62ea556abefb9a71ae07c6d27ba319
Vinicento <105458944+Vinicento@users.noreply.github.com>
2023-07-21 09:17:08 +0200

    Update Gui_nfz_web_scraping.py

--- Gui_nfz_web_scraping.py
+++ Gui_nfz_web_scraping.py
@@ -1,30 +1,27 @@
+
 import tkinter as tk
 import tkinter.font as tkFont
 from tkinter import *
-from functools import partial
-
 import scrap_functions
-
-print(dir(scrap_functions.basic_searches))
 
 
 class App:
     def __init__(self, root):
-        # setting title
+
         self.root = root
         self.root.title("undefined")
-        # setting window size
+
         self.start_arguments = []
         self.width = 600
         self.height = 400
         self.screenwidth = root.winfo_screenwidth()
         self.screenheight = root.winfo_screenheight()
         self.alignstr = '%dx%d+%d+%d' % (
-        self.width, self.height, (self.screenwidth - self.width) / 2, (self.screenheight - self.height) / 2)
+            self.width, self.height, (self.screenwidth - self.width) / 2, (self.screenheight - self.height) / 2)
         self.root.geometry(self.alignstr)
         self.root.resizable(width=False, height=False)
         self.root.configure(bg='#ffdfb3')
-        self.ft = tkFont.Font(family='Times', size=16, weight='bold')  # Specify font weight as 'bold'
+        self.ft = tkFont.Font(family='Times', size=16, weight='bold')
 
         self.GLabel_40 = tk.Label(root)
         self.GLabel_40["bg"] = "#ffdfb3"
@@ -36,7 +33,7 @@ class App:
         self.GLabel_40["font"] = self.ft
 
         self.GLabel_40.place(x=330, y=20, width=256, height=52)
-        #
+
         self.data_od_placeholder = tk.Entry(root)
         self.data_od_placeholder["bg"] = "#713310"
         self.data_od_placeholder["borderwidth"] = "1px"
@@ -46,8 +43,6 @@ class App:
         self.data_od_placeholder["justify"] = "center"
         self.data_od_placeholder.insert(0, "2020")
         self.data_od_placeholder["font"] = self.ft
-
-        # self.data_od_placeholder.place(x=470,y=120,width=70,height=25)
 
         self.data_do_placeholder = tk.Entry(root)
         self.data_do_placeholder["bg"] = "#713310"
@@ -59,9 +54,6 @@ class App:
         self.data_do_placeholder.insert(0, "2022")
         self.data_do_placeholder["font"] = self.ft
 
-        # self.data_do_placeholder.place(x=380,y=120,width=70,height=25)
-
-        # Label lata od do
         self.label_od = tk.Label(root)
         self.label_od["bg"] = "#ffdfb3"
         self.ft = tkFont.Font(family='Times', size=10)
@@ -71,8 +63,6 @@ class App:
         self.label_od["text"] = "Od:"
         self.label_od["font"] = self.ft
 
-        # self.label_od.place(x=380,y=90,width=70,height=25)
-
         self.label_do = tk.Label(root)
         self.label_do["bg"] = "#ffdfb3"
         self.ft = tkFont.Font(family='Times', size=10)
@@ -81,10 +71,7 @@ class App:
         self.label_do["justify"] = "center"
         self.label_do["text"] = "Do"
         self.label_do["font"] = self.ft
-        # self.label_do.place(x=470,y=90,width=70,height=25)
-        #
 
-        # aktywator
         self.GButton_438 = tk.Button(root)
         self.GButton_438["activebackground"] = "#a42828"
         self.GButton_438["bg"] = "#713310"
@@ -106,12 +93,10 @@ class App:
             self.GButton_438["bg"] = self.GButton_438["activebackground"]
             self.GButton_438["fg"] = self.GButton_438["activeforeground"]
 
-        # Function to handle mouse leave event
         def on_leave(event):
             self.GButton_438["bg"] = "#713310"
             self.GButton_438["fg"] = "#f7c379"
 
-        # Bind the enter and leave events to the button
         self.GButton_438.bind("<Enter>", on_enter)
         self.GButton_438.bind("<Leave>", on_leave)
 
@@ -126,7 +111,6 @@ class App:
         self.path_input["font"] = self.ft
         self.path_input.place(x=260, y=320, width=316, height=30)
 
-        # scieżka pliku
         self.GLabel_420 = tk.Label(root)
         self.GLabel_420["bg"] = "#ffdfb3"
         self.ft = tkFont.Font(family='Times', size=10)
@@ -137,7 +121,6 @@ class App:
         self.GLabel_420["font"] = self.ft
         self.GLabel_420.place(x=260, y=290, width=315, height=30)
 
-        # branches
         self.branches_label = tk.Label(root)
         self.branches_label["bg"] = "#ffdfb3"
         self.ft = tkFont.Font(family='Times', size=10)
@@ -146,8 +129,6 @@ class App:
         self.branches_label["justify"] = "center"
         self.branches_label["text"] = "Wojewodztwa:"
         self.branches_label["font"] = self.ft
-
-        # self.branches_label.place(x=360,y=170,width=77,height=30)
 
         self.branches_placeholder = tk.Entry(root)
         self.branches_placeholder["bg"] = "#713310"
@@ -159,9 +140,6 @@ class App:
         self.branches_placeholder.insert(0, "01,02,03,04,05,06,07,08,09,10,11,12,13,14,15,16")
         self.branches_placeholder["font"] = self.ft
 
-        # self.branches_placeholder.place(x=360,y=200,width=77,height=30)
-
-        # niestałe kod produktu
         self.label_produkty = tk.Label(root)
         self.label_produkty["bg"] = "#ffdfb3"
         self.ft = tkFont.Font(family='Times', size=10)
@@ -170,8 +148,6 @@ class App:
         self.label_produkty["justify"] = "center"
         self.label_produkty["text"] = "Produkty"
         self.label_produkty["font"] = self.ft
-
-        # self.label_produkty.place(x=460,y=170,width=116,height=30)
 
         self.produkty_placeholder = tk.Entry(root)
         self.produkty_placeholder["bg"] = "#713310"
@@ -183,9 +159,6 @@ class App:
         self.produkty_placeholder["text"] = "03.0000.335.02"
         self.produkty_placeholder["font"] = self.ft
 
-        # self.produkty_placeholder.place(x=460,y=200,width=116,height=30)
-        ###########
-        # niestałe kod produktu
         self.label_usluga = tk.Label(root)
         self.label_usluga["bg"] = "#ffdfb3"
         self.ft = tkFont.Font(family='Times', size=10)
@@ -194,8 +167,6 @@ class App:
         self.label_usluga["justify"] = "center"
         self.label_usluga["text"] = "Uslugi"
         self.label_usluga["font"] = self.ft
-
-        # self.label_usluga.place(x=360,y=170,width=116,height=30)
 
         self.usluga_placeholder = tk.Entry(root)
         self.usluga_placeholder["bg"] = "#713310"
@@ -207,9 +178,6 @@ class App:
         self.usluga_placeholder.insert(0, "01,02,03,04,05,06,07,08,09,10,11,12,13,14,15,16,17,18")
         self.usluga_placeholder["font"] = self.ft
 
-        # self.produkty_usluga.place(x=360,y=200,width=116,height=30)
-
-        # niestałe kod produktu
         self.icd9_produkty = tk.Label(root)
         self.icd9_produkty["bg"] = "#ffdfb3"
         self.ft = tkFont.Font(family='Times', size=10)
@@ -218,8 +186,6 @@ class App:
         self.icd9_produkty["justify"] = "center"
         self.icd9_produkty["text"] = "kod icd9"
         self.icd9_produkty["font"] = self.ft
-
-        # self.label_produkty.place(x=460,y=170,width=116,height=30)
 
         self.icd9_placeholder = tk.Entry(root)
         self.icd9_placeholder["bg"] = "#713310"
@@ -231,7 +197,6 @@ class App:
         self.icd9_placeholder["text"] = "wpisz kody"
         self.icd9_placeholder["font"] = self.ft
 
-        # self.produkty_placeholder.place(x=460,y=200,width=116,height=30)
         self.GLabel_198 = tk.Label(root)
         self.GLabel_198["bg"] = "#ffdfb3"
         self.ft = tkFont.Font(family='Times', size=10)
@@ -264,12 +229,12 @@ class App:
         self.settings_1 = [[self.label_do, 470, 90, 70, 25], [self.data_od_placeholder, 380, 120, 70, 25],
                            [self.data_do_placeholder, 470, 120, 70, 25], [self.label_od, 380, 90, 70, 25],
                            [self.branches_label, 360, 170, 77, 30], [self.branches_placeholder, 360, 200, 77, 30]]
-        # Replace with your actual January widgets
+
         self.settings_2 = [[self.label_do, 470, 90, 70, 25], [self.data_od_placeholder, 380, 120, 70, 25],
                            [self.data_do_placeholder, 470, 120, 70, 25], [self.label_od, 380, 90, 70, 25],
                            [self.label_produkty, 460, 170, 116, 30], [self.produkty_placeholder, 460, 200, 116, 30],
                            [self.branches_label, 360, 170, 77, 30], [self.branches_placeholder, 360, 200, 77, 30]]
-        # Replace with your actual February widgets
+
         self.settings_3 = [[self.icd9_produkty, 460, 170, 116, 30], [self.icd9_placeholder, 460, 200, 116, 30]]
 
         self.settings_4 = [[self.label_do, 470, 90, 70, 25], [self.data_od_placeholder, 380, 120, 70, 25],
@@ -323,19 +288,18 @@ class App:
 
         elif self.selected_option == "kwota_kontraktu_dla_produktu":
             self.current_widgets = self.settings_4
-            self.start = scrap_functions.basic_searches.kwota_kontraktu_dla_produktu  # TODO dodac usluge
+            self.start = scrap_functions.basic_searches.kwota_kontraktu_dla_produktu
             self.start_arguments = [self.data_od_placeholder.get(), self.data_do_placeholder.get(),
                                     list(self.branches_placeholder.get().split(",")),
                                     list(self.produkty_placeholder.get()), self.path_input.get()]
 
         elif self.selected_option == "Kontrakty_szpitale_produkty_wszystkie_uslugi":
             self.current_widgets = self.settings_4
-            self.start = scrap_functions.basic_searches.kwota_kontraktów  # TODO dodac usluge
+            self.start = scrap_functions.basic_searches.kwota_kontraktów
             self.start_arguments = [self.data_od_placeholder.get(), self.data_do_placeholder.get(),
                                     list(self.branches_placeholder.get().split(",")),
                                     list(self.produkty_placeholder.get()), self.path_input.get()]
 
-        # Show widgets for the current selection
         self.show_widgets(self.current_widgets)
 
     def activator(self, arguments):
@@ -361,7 +325,6 @@ class App:
                                     int(self.data_do_placeholder.get()),
                                     list(self.branches_placeholder.get().split(",")), self.path_input.get()]
 
-        print(self.start_arguments)
         self.start(*self.start_arguments)
 
 
@@ -369,4 +332,3 @@ if __name__ == "__main__":
     root = tk.Tk()
     app = App(root)
     root.mainloop()
-
